9d2fb2ece49adea720e3fbf320af91f0f4560ee3
Maurits Wilke <mauritswilke@gmail.com>
2021-04-28 17:12:48 +0200

    removed testutils.js

--- testutils.js
+++
@@ -1,9 +0,0 @@
-const utils = require('./utils.js');
-
-(async()=>{
-        const player = await utils.returnHypixelData("de_grote")
-        .then(p => {
-            console.log(p.player.displayname)
-        })
-        .catch(e => {return console.log(e);});
-})()
